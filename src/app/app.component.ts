5ac632ff3ed000d44327a91cf991fd86b213ab9e
Priya K Nair <priyak.twinkle@gmail.com>
2025-05-20 17:55:55 +0200

    basic grid functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,41 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'boggle';
+  letterArr: string[] = [];
+  word="";
+  wordArr: string[] = [];
+
+
+  ngOnInit(): void {
+      this.createLetters();
+  }
+
+  createLetters() {
+  for (let i=1; i<17; i++) { //for 16 dice values
+    let  ascii = Math.floor(Math.random() * 25) + 65;  //65-90 ASCII values of A-Z, code referred from https://coreui.io/blog/how-to-generate-a-random-number-in-javascript/
+    let alpha = String.fromCharCode(ascii);
+    console.log(alpha);
+    this.letterArr.push(alpha);
+  }
+  
 }
+
+captureLetter(event: any) {
+this.word=this.word+event.target.textContent;
+}
+
+enterWord() {
+  if (this.word.length > 2) {
+    this.wordArr.push(this.word);
+  }
+  this.word="";
+}
+
+
+}
+
+
+
